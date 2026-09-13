5c9ac2125617132aafbae0eac28161f76dc071c7
KHSolloway <KHSolloway@my.waketech.edu>
2026-09-13 17:08:47 -0400

    Mod 4 Assignment - Created weekly goal function and form

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ This project is a web-based dashboard built for WEB-115 to demonstrate interacti
 - [ ] Add a metric conversion tool.
 - [ ] Integrate a task list with array storage.
 - [ ] Add JavaScript logic for a live clock.
+- [x] Add a weekly task goal calculator.
 
+
+## Weekly Task Goals
+The Weekly Task Goal Calculator takes the Daily Task Goal, multiplies it by 5 (5 days/week) to calculate the Weekly Goal. It then adds the Weekly Bonus Tasks to the  Weekly Goal and displays the Total Weekly Tasks.
---
+++ js/task-manager.js
@@ -0,0 +1,27 @@
+
+function weeklyGoal(userName, dailyGoal, bonusTasks) {
+
+    let weeklyTotal = dailyGoal * 5;
+    //FIXED changed weeklyGoal to WeeklyTotal
+    let totalGoal = weeklyTotal + bonusTasks;
+
+    let output = userName + " has a weekly goal of " + totalGoal + " tasks.";
+
+    document.getElementById("goal-message").innerHTML = output;
+
+}
+
+document.getElementById("goal-btn").addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    let userName = document.getElementById("userName").value;
+    let dailyGoal = Number(document.getElementById("dailyGoal").value);
+    //FIXED getElementsByID to getElementByID
+    let bonusTasks = Number(document.getElementById("bonusTasks").value);
+
+
+    weeklyGoal(userName, dailyGoal, bonusTasks);
+})
+
+
